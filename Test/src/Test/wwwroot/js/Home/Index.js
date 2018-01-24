3a008cda1e5aec43492913e1a9efd903d9ee7781
DESKTOP-LPDU7FA\achuc <agatachucherko@gmail.com>
2018-01-24 17:21:49 +0100

    Adding a Google Map

---
+++ Test/src/Test/wwwroot/js/Home/Index.js
@@ -0,0 +1,13 @@
+﻿console.log("ready!");
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 50.059306, lng: 19.928298 },
+        zoom: 12
+    });
+    var marker = new google.maps.Marker({
+        position: { lat: 50.069678, lng: 19.909633 },
+        map: map,
+        title: "Nawojki!"
+    });
+}
